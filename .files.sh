22d9825970259418ff631e47c96e263bcc753c3b
Alex Raber <alexhraber@gmail.com>
2017-01-06 02:27:00 -0800

    removed zsh for now

--- .files.sh
+++ .files.sh
@@ -2,7 +2,7 @@
 
 dir=~/.dotfiles
 odir=$dir/old
-files="bashrc zshrc oh-my-zsh xinitrc gitconfig i3"
+files="bashrc i3 xinitrc gitconfig"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $odir
@@ -11,20 +11,6 @@ for f in $files; do
     mv ~/.$f $odir
     ln -s $dir/$f ~/.$f
 done
-
-install_zsh () {
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    sudo pacman -S zsh
-    install_zsh
-fi
-}
 
 install_i3 () {
     sudo pacman -S i3-wm i3 i3lock i3status
@@ -44,7 +30,6 @@ fi
 }
 
 . ~/.bashrc
-install_zsh
 install_aurora
 install_i3
 
