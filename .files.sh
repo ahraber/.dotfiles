f8e09dd9a94acdc95c9afaffd21575b3357fdb37
Alex Raber <alexhraber@gmail.com>
2017-02-22 02:00:14 -0800

    updated .files.sh

--- .files.sh
+++ .files.sh
@@ -2,7 +2,7 @@
 
 dir=~/.dotfiles
 odir=$dir/old
-files="bashrc i3 screenlayout xinitrc gitconfig"
+files="bashrc i3 fehbg screenlayout synergy.conf vimrc xinitrc gitconfig"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $odir
